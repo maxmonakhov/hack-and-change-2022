3b44310fc87972c79c15857b95b5a37e93290d3a
Maxim Monakhov <maxim@superdao.co>
2022-11-12 15:27:57 +0400

    initial project setup

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "plugins": [
+    require("prettier-plugin-tailwindcss")
+  ],
+  "printWidth": 80,
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true,
+  "quoteProps": "as-needed",
+  "jsxSingleQuote": false,
+  "trailingComma": "none",
+  "bracketSpacing": true,
+  "arrowParens": "always",
+  "endOfLine": "lf",
+}
